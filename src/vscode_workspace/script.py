e7b836b7a1c8fcb11c58054e24cf58a714eb7094
jimbiscuit <julienchandelle@gmail.com>
2022-02-28 14:39:15 +0100

    Add coding tag, add check if is there a src folder and add package check

--- src/vscode_workspace/script.py
+++ src/vscode_workspace/script.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import os
 
@@ -8,7 +9,10 @@ def main():
     curr_path = os.path.realpath(os.path.curdir)
     src = "src"
     path = os.path.join(curr_path, src)
-    list_of_package = os.listdir(path)
+    try:
+        list_of_package = os.listdir(path)
+    except OSError:
+        raise OSError("Could not find src folder")
 
     # get folder name
 
@@ -19,6 +23,9 @@ def main():
     dict_to_json = {"folders": [{"path": "."}], "settings": {}}
 
     for package in list_of_package:
+        # check if folder is a python package
+        if not os.path.isfile(os.path.join(path, package, "setup.py")):
+            continue
         dict_to_json["folders"].append({"path": "src/" + package})
 
     # dump to json
