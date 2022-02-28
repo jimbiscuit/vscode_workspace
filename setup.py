5d2df751403a5d348ae490a9e76b5c25e37adc3f
jimbiscuit <julienchandelle@gmail.com>
2022-02-28 13:56:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="vscode-workspace",
+    version="0.0.1",
+    author="Jimbiscuit",
+    author_email="julienchandelle@gmail.com",
+    description="Generate vscode workspace file",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    entry_points={
+        "console_scripts": [
+            "vsc-init=vscode_workspace.script:main"
+        ]
+    }
+)
